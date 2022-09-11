ba8ee6c57a3f9d581c3ec763fe7f1c5d017cd2da
khunzinshine <khunzinshine@ayainnovation.com>
2022-09-11 22:44:21 +0630

    change bg color

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import products from "./data/products.json";
 
 function App() {
   return (
-    <div className="bg-gray-200 py-4 dark:bg-gray-800">
+    <div className="bg-cyan-50 py-4 gray:bg-gray-800">
       <div className="md:w-1/2 mx-2 md:mx-auto">
         {products.map((product) => (
           <Product product={product} key={product.id} />
